d75d3247d68816e1ce092c2cf2579d8c6f6e58b9
Sonica Pou <sonica.pou2@gmail.com>
2026-08-27 00:02:10 -0700

    Add NBA player data collection script

---
+++ get_data.py
@@ -0,0 +1,15 @@
+from nba_api.stats.endpoints import leaguedashplayerstats
+
+import pandas as pd
+
+stats = leaguedashplayerstats.LeagueDashPlayerStats(
+    season="2025-26",
+    season_type_all_star="Regular Season"
+)
+
+df = stats.get_data_frames()[0]
+
+print(df.head())
+print(df.columns.tolist())
+
+df.to_csv("data/nba_player_stats_2025_26.cvs", index=False)
